0fa9686897fdb72e3433e3b7e18d1ec54a763148
Stevexd100 <steve.lopez@upaep.mx>
2026-04-23 14:26:34 +0000

    Se genero todo el codigo

--- Perro.cs
+++ Perro.cs
@@ -1,1 +1,35 @@
 
+class Perro : Animal
+{
+    public string Raza {get; set;}
+
+    public Perro(string nombre, int edad, string raza)
+    :base(nombre, edad)
+    {
+        Raza = raza ;
+    }
+
+    public new void HacerSonido()
+    {
+        Console.WriteLine(Nombre + " dice: ¡Guau!");
+    }
+
+    public void Buscar()
+    {
+        Console.WriteLine(Nombre + " esta buscando algo...");
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        Perro perrito =new Perro(" Troquito",5, "Pug");
+        perrito.MostrarInfo();
+        perrito.HacerSonido();
+        perrito.Buscar();
+
+        Console.WriteLine(" Raza: "+ perrito.Raza);
+    }
+
+}
